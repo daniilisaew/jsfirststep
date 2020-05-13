b92584f93143af3f875d7a94039af932942c1eb9
daniilisaew <57032484+daniilisaew@users.noreply.github.com>
2020-05-13 19:01:58 +0300

    Js #1

---
+++ js.js
@@ -0,0 +1,52 @@
+  let text;
+  let regexp;
+  let asd;
+  let paramA;
+  let paramB;  
+  text = 'This was hard. \n But I did it. \n And its all.';
+
+     function tel()
+     {
+       exp = text.toLowerCase();
+       return exp;
+       //text.toLowerCase();
+     }
+
+     function ler() 
+      {     
+         regexp = /\w/g;  
+         asd = exp.match(regexp);
+         return asd
+         //text.match(regexp);
+      }
+      function dep() {
+       aps = asd.join('');
+       return aps 
+      }
+      function fer() {
+        sgg = aps.match(/.{1,4}/g)
+        return sgg
+      }
+
+      function daf() {
+        sgd = sgg.length
+        return sgd
+      }
+      function dek() {
+        sgf = aps.match(/.{1,4}/g).join(' ')
+        return sgf
+      }
+
+
+        paramA = tel();
+        paramB = ler(paramA);
+        paramC = dep(paramB);
+        paramD = fer(paramC);
+        paramE = daf(paramD);
+        paramF = dek(paramE);
+
+
+console.log(paramC);
+console.log(paramF);
+console.log(paramD);
+console.log(paramE);
